a2ffd583084898c64987b03d484344a65ac215fb
adhipratama <yogaadhi@icloud.com>
2021-12-07 20:24:21 +0700

    adding product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,6 +31,48 @@ const getAllProducts = async (req, res) => {
 
 const getOneProduct = async (req, res) => {
   let id = req.params.id;
-  let product = await Product.findOne({ where: {id: id} });
+  let product = await Product.findOne({ where: { id: id } });
   res.status(200).send(product);
+};
+
+//update product ===========================
+
+const updateProduct = async (req, res) => {
+  let id = req.params.id;
+
+  const product = await Product.update(req.body, { where: { id: id } });
+
+  res.status(200).send(product);
+};
+
+//delete product by id ======================
+
+const deleteProduct = async (req, res) => {
+  let id = req.params.id;
+
+  await Product.destroy({ where: { id: id } });
+
+  res.status(200).send("successfully deleted....");
+};
+
+//publish product ==============================
+
+const getPublishedProduct = async (req, res) => {
+    let id = req.params.id;
+  
+    const products = await Product.findAll({ where: { published: true } });
+  
+    res.status(200).send("successfully deleted....");
+  };
+
+
+
+
+module.expoerts = {
+  addProduct,
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  deleteProduct,
+  getPublishedProduct
 };
